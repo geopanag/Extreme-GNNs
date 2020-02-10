280597eeed7da252aa591c24af6297bf6229f2fc
George <georgepanagopoulos5@yahoo.gr>
2020-02-09 19:47:55 -0500

    10 experiments and new definition of labels

--- code/eggn_v2.py
+++ code/eggn_v2.py
@@ -35,7 +35,7 @@ import operator
 
 George = True #if True, George is running the code, otherwise, Hamid is working on the code
 if George:
-    os.chdir("/home/george/Desktop/Extreme-GNNs/code") 
+    os.chdir("/home/george/Desktop/extreme-gnns/code") 
 else:
     os.chdir("/home/h/Documents/Japet/Extreme-GNNs/code")
 
@@ -96,13 +96,13 @@ def train(dat):
 def test(dat):
     model.eval()
     logits, accs = model(dat), []
-    for _, mask in dat('train_mask', 'test_mask'):
+    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
         pred = logits[mask].max(1)[1]
-        acc = pred.eq(dat.y[mask]).sum().item() / mask.sum().item()
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
         accs.append(acc)
-    return accs
+    return accs  
 
-    
+
 def get_label(train_extremes,y):
     d = dict(Counter([y[i] for i in np.where(train_extremes)[0]]))
     return  max(d.items(), key=operator.itemgetter(1))[0]
@@ -110,7 +110,7 @@ def get_label(train_extremes,y):
     
 
 if George:
-    os.chdir("/home/george/Desktop/Extreme-GNNs/data") 
+    os.chdir("/home/george/Desktop/extreme-gnns/data") 
 else:
     os.chdir("/home/h/Documents/Japet/Extreme-GNNs/data")
            
@@ -118,7 +118,9 @@ if __name__ == '__main__':
     #------- Dataset & Parameters
     hidden_size = 20
     lr = 0.01
-    n_epochs = 300
+    n_epochs = 200
+    experiments = 10
+    
     
     thresholds = eval(open('degree_thresholds.txt' , 'r').read())
             
@@ -143,139 +145,171 @@ if __name__ == '__main__':
     
     #plt.hist([data.y.numpy()[i] for i in np.where(data.val_mask.numpy())[0]])
     
+    
     labs =  data.y.numpy()
     #labs = labs.set_index("node")
     num_classes = len(np.unique(labs))
-
     #x = np.load("cora_features.npy")
     #num_features = x.shape[1]
     thres = thresholds["cora-net"]
-                
     extremes = [True  if i>thres else False for i in degs] 
     regs = [True  if i<=thres else False for i in degs] 
     
-    train_samples = [names[i] for i in np.where(data.train_mask.numpy())[0]]#np.random.choice( [i for i in range(len(names))] ,  round(20*len(names)/100),replace=False) 
-    
-    test_samples = [names[i] for i in np.where(data.test_mask.numpy())[0]]
-    del data.val_mask
-    
-    train_extremes, train_regulars =  get_indices(train_samples,dic,thres)
-    test_extremes, test_regulars =  get_indices(test_samples,dic,thres)
+    #-----------------------------------------------
 
-    y = data.y.numpy()
-    extreme_train_label = get_label(train_extremes,y)
+    logw = open("logw.csv","w")
+    logw.write("test_r,test_e,test_br,test_be\n")
+                
+    for i in range(experiments):
+        train_samples = [names[i] for i in np.where(data.train_mask.numpy())[0]]#np.random.choice( [i for i in range(len(names))] ,  round(20*len(names)/100),replace=False) 
+        val_samples = [names[i] for i in np.where(data.val_mask.numpy())[0]]#np.random.choice( [i for i in range(len(names))] ,  round(20*len(names)/100),replace=False) 
+        test_samples = [names[i] for i in np.where(data.test_mask.numpy())[0]]
+        #del data.val_mask
+    
+        print("train")    
+        train_extremes, train_regulars =  get_indices(train_samples,dic,thres)
+        print("val")    
+        val_extremes, val_regulars =  get_indices(val_samples,dic,thres)
+        print("test")    
+        test_extremes, test_regulars =  get_indices(test_samples,dic,thres)
+    
+        #y = data.y.numpy()
+        #plt.hist([y[int(i)] for i in val_samples])
+    
+        #sum(test_extremes)
+        y = data.y.numpy()
+        extreme_train_label = get_label(train_extremes,y)
+        print(extreme_train_label)
         
+        data.y = torch.tensor(np.array([i==1 for i in list(y)]).astype(int))
+        
+        #train_idx = list(data.train_mask.numpy())
+        #test_idx = list(data.test_mask.numpy())
+        #val_idx = list(data.val_mask.numpy())
+        #y = list(data.y.numpy())
+        #labs = [y[i] for i in np.where(train_idx)[0]]
+        #vals = [y[i] for i in np.where(val_idx)[0]]
+        #vals = [y[i] for i in np.where(test_idx)[0]]
+        #plt.hist(vals)
+        #------- Create the input to the algorithm
+        #source = []
+        #target = []
+        #for i in g.es():
+        #    source.append(i.tuple[0])
+        #    target.append(i.tuple[1])
+        #edge_index = torch.tensor([source,target])
+        
+        #------- Input to the extreme head
+        train_mask_e = torch.tensor(train_extremes)
+        val_mask_e = torch.tensor(val_extremes)
+        test_mask_e =  torch.tensor(test_extremes)
+        
+        
+        #--- Input to the regular head 
+        train_mask_r = torch.tensor(train_regulars)
+        val_mask_r = torch.tensor(val_regulars)
+        test_mask_r =  torch.tensor(test_regulars)#[not i for i in test_extremes])
+        
+        #------- Define the input variables 
+        #y = torch.tensor(labs["label"].values.tolist())
+        #x = torch.tensor(x)    
     
-    data.y = torch.tensor(np.array([i==1 for i in list(y)]).astype(int))
+        num_features = data.x.shape[1]
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        #------------------------------ Regular
+        dat_regular = Data(edge_index = data.edge_index, test_mask = test_mask_r, 
+                    train_mask = train_mask_r, x = data.x , y = data.y, val_mask = val_mask_r)    
+        
+        model, dat_regular = gcn(num_features,num_classes,hidden_size).to(device), dat_regular.to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
+        
+        print("regular")    
+        best_test_r = 0
+        best_val_acc = test_acc = 0
+        for epoch in range(1, n_epochs+1):
+            train(dat_regular)
+            train_acc, val_acc, tmp_test_acc = test(dat_regular)
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                test_acc = tmp_test_acc
+            if(test_acc>best_test_r):
+                best_test_r = test_acc   
+            if epoch%50==0:
+                log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+                print(log.format(epoch, train_acc, best_val_acc, test_acc))
+            
+            
+        #------------------------------ Extreme
+        dat_extreme = Data(edge_index = data.edge_index, test_mask = test_mask_e, 
+                    train_mask = train_mask_e, x = data.x , y = data.y).to(device)   
+        
+        model = gcn(num_features,num_classes,hidden_size).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
+        
+        
+        print("extreme")    
+        best_test_e = 0
+        best_val_acc = test_acc = 0
+        for epoch in range(1, n_epochs+1):
+            train(dat_extreme)
+            train_acc, val_acc, tmp_test_acc = test(dat_extreme)
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                test_acc = tmp_test_acc
+                if(test_acc>best_test_e):
+                    best_test_e = test_acc   
+            if epoch%50==0:
+                log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+                print(log.format(epoch, train_acc, best_val_acc, test_acc))   
+            
+        
+        
+        #---------------------- Base Regular
+        dat_baseline_reg = Data(edge_index = data.edge_index, test_mask = test_mask_r, 
+                    train_mask = data.train_mask, x = data.x , y = data.y).to(device)   
+        
+        model = gcn(num_features,num_classes,hidden_size).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
+        
+        
+        print("baseline regular")
+        best_test_br = 0
+        best_val_acc = test_acc = 0
+        for epoch in range(1, n_epochs+1):
+            train(dat_baseline_reg)
+            train_acc, val_acc, tmp_test_acc = test(dat_baseline_reg)
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                test_acc = tmp_test_acc
+                if(test_acc>best_test_br):
+                    best_test_br = test_acc   
+            if epoch%50==0:
+                log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+                print(log.format(epoch, train_acc, best_val_acc, test_acc))  
     
-    #train_idx = list(data.train_mask.numpy())
-    #test_idx = list(data.test_mask.numpy())
-    #val_idx = list(data.val_mask.numpy())
-    #y = list(data.y.numpy())
-    #labs = [y[i] for i in np.where(train_idx)[0]]
-    #vals = [y[i] for i in np.where(val_idx)[0]]
-    #vals = [y[i] for i in np.where(test_idx)[0]]
-    #plt.hist(vals)
-    #------- Create the input to the algorithm
-    #source = []
-    #target = []
-    #for i in g.es():
-    #    source.append(i.tuple[0])
-    #    target.append(i.tuple[1])
-    #edge_index = torch.tensor([source,target])
-    
-    #------- Input to the extreme head
-    train_mask_e = torch.tensor(train_extremes)
-    test_mask_e =  torch.tensor(test_extremes)
-    
-    
-    #--- Input to the regular head 
-    train_mask_r = torch.tensor(train_regulars)
-    test_mask_r =  torch.tensor(test_regulars)#[not i for i in test_extremes])
-    
-    #------- Define the input variables 
-    #y = torch.tensor(labs["label"].values.tolist())
-    #x = torch.tensor(x)    
-
-    num_features = data.x.shape[1]
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    
-    #------------------------------ Regular
-    dat_regular = Data(edge_index = data.edge_index, test_mask = test_mask_r, 
-                train_mask = train_mask_r, x = data.x , y = data.y)    
-    
-    model, dat_regular = gcn(num_features,num_classes,hidden_size).to(device), dat_regular.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
-    
-    print("regular")    
-    best_test_acc = test_acc = 0
-    for epoch in range(1, n_epochs+1):
-        train(dat_regular)
-        train_acc, tmp_test_acc = test(dat_regular)
-        if tmp_test_acc > best_test_acc:
-            test_acc = tmp_test_acc
-        log = 'Epoch: {:03d}, Train: {:.4f},  Test: {:.4f}'
-        if epoch%50==0:
-            print(log.format(epoch, train_acc, test_acc))
-   
-    
-    #------------------------------ Extreme
-    dat_extreme = Data(edge_index = data.edge_index, test_mask = test_mask_e, 
-                train_mask = train_mask_e, x = data.x , y = data.y).to(device)   
-    
-    model = gcn(num_features,num_classes,hidden_size).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
-    
-    print("extreme")    
-    best_test_acc = test_acc = 0
-    for epoch in range(1, n_epochs+1):
-        train(dat_extreme)
-        train_acc, tmp_test_acc = test(dat_extreme)
-        if tmp_test_acc > best_test_acc:
-            test_acc = tmp_test_acc
-        log = 'Epoch: {:03d}, Train: {:.4f},  Test: {:.4f}'
-        if epoch%50==0:
-            print(log.format(epoch, train_acc, test_acc))
+        #---------------------- Base Extreme
+        dat_baseline_ex = Data(edge_index = data.edge_index, test_mask = test_mask_e, 
+                    train_mask = data.train_mask, x = data.x , y = data.y).to(device)   
+            
+        model = gcn(num_features,num_classes,hidden_size).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
        
+        print("baseline extreme")
+        best_test_be = 0
+        best_val_acc = test_acc = 0
+        for epoch in range(1, n_epochs+1):
+            train(dat_baseline_ex)
+            train_acc, val_acc, tmp_test_acc = test(dat_baseline_ex)
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                test_acc = tmp_test_acc
+                if(test_acc>best_test_be):
+                    best_test_be = test_acc                
+            if epoch%50==0:
+                log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+                print(log.format(epoch, train_acc, best_val_acc, test_acc))  
         
-    
-    #---------------------- Base Regular
-    dat_baseline_reg = Data(edge_index = data.edge_index, test_mask = test_mask_r, 
-                train_mask = data.train_mask, x = data.x , y = data.y).to(device)   
-    
-    model = gcn(num_features,num_classes,hidden_size).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
-    
-    
-    print("baseline regular")
-    best_test_acc = test_acc = 0
-    for epoch in range(1, n_epochs+1):
-        train(dat_baseline_reg)
-        train_acc, tmp_test_acc = test(dat_baseline_reg)
-        if tmp_test_acc > best_test_acc:
-            test_acc = tmp_test_acc
-        log = 'Epoch: {:03d}, Train: {:.4f},  Test: {:.4f}'
-        if epoch%50==0:
-            print(log.format(epoch, train_acc, test_acc))
-
+        logw.write(str(best_test_r)+","+str(best_test_e)+"," +str(best_test_br)+","+str(best_test_be)+"\n")
         
-    #---------------------- Base Extreme
-    dat_baseline_ex = Data(edge_index = data.edge_index, test_mask = test_mask_e, 
-                train_mask = data.train_mask, x = data.x , y = data.y).to(device)   
-        
-    model = gcn(num_features,num_classes,hidden_size).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
-   
-    print("baseline extreme")
-    best_test_acc = test_acc = 0
-    for epoch in range(1, n_epochs+1):
-        train(dat_baseline_ex)
-        train_acc, tmp_test_acc = test(dat_baseline_ex)
-        if tmp_test_acc > best_test_acc:
-            test_acc = tmp_test_acc
-        log = 'Epoch: {:03d}, Train: {:.4f},  Test: {:.4f}'
-        if epoch%50==0:
-            print(log.format(epoch, train_acc, test_acc))
-    
-    
+logw.close()
