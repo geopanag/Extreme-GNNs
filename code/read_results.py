8436eb382da62f6fee2a0c774847a28738dfae64
George <georgepanagopoulos5@yahoo.gr>
2020-03-24 19:08:50 -0400

    final results and hyper opt

--- code/read_results.py
+++ code/read_results.py
@@ -12,20 +12,26 @@ import os.path as osp
 import argparse
 import numpy as np
 
-os.chdir("/home/george/Desktop/extreme-gnns/results_new") 
 
+os.chdir("/home/george/Desktop/extreme-gnns/log") 
 
-for mod in ["gat","gcn","sage"]:
-    for ds in  ['CiteSeer','Cora','PubMed']:
-        df = pd.read_csv("logw_"+mod+"_"+ds+".csv")
-        df["perc"]= df["perc"]+4
+for ext in ["test","val"]:
+    for mod in ["gat","gcn","sage"]:
+        for ds in  ['CiteSeer','Cora','PubMed']:
+            df = pd.read_csv("logw_"+ ext+"_"+mod+"_"+ds+".csv")
+            #"logw_"+ ext+"_"+mod+"_"+str(i)+"_"+ds+"
+            df["perc"]= df["perc"]+2
+            
+            try:
+                reg = df["test_r"]-df["test_br"]
+                ex = df["test_e"]-df["test_be"]
+            except:
+                reg = df["val_r"]-df["val_br"]
+                ex = df["val_e"]-df["val_be"]
+            df["diff_r"] = reg
+            df["diff_e"] = ex
+            df.to_csv("../results/res_"+ ext+"_"+mod+"_"+ds+".csv",index=False)
         
-        reg = df["test_r"]-df["test_br"]
-        ex = df["test_e"]-df["test_be"]
-        df["diff_r"] = reg
-        df["diff_e"] = ex
-        df.to_csv("../results_new/res_"+mod+"_"+ds+".csv",index=False)
-    
       
     
     
